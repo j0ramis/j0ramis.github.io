92d6efc26d11fb2ba669d9eb0f91c073fac9f62d
j0ramis <jgutie17@charlotte.edu>
2026-05-28 01:51:28 -0400

    Fixed formatting and JS issues

---
+++ itis3135/scripts/defaultscripts.js
@@ -0,0 +1,12 @@
+document.getElementById("validation-link-html").setAttribute(
+    "href",
+    "https://validator.w3.org/check?uri=" + location.href
+);
+
+document.getElementById("validation-link-css").setAttribute(
+    "href",
+    "https://jigsaw.w3.org/css-validator/validator?uri=" + location.href
+);
+
+
+document.getElementById("current-year").textContent = new Date().getFullYear();
